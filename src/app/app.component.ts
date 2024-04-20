d468c18a38869d4016a7910145f99d3481d4ff1f
Kiran05041988 <140429393+Kiran05041988@users.noreply.github.com>
2024-04-20 11:08:29 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'this modify link!!!';
+  title = 'this modify link  for projrct!!!';
 }
